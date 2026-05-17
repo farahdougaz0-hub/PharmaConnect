5ce255b6304ea0a094105740f843963326bea661
abkdoua <abkdoua@gmail.com>
2026-05-17 14:18:07 +0100

    Update mes-reservations.php

--- reservations/mes-reservations.php
+++ reservations/mes-reservations.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-require_once "../../config/db.php";
+require_once "../config/db.php";
 
 if (!isset($_SESSION['user_id'])) {
-    header("Location: ../../login.php");
+    header("Location: ../login.php");
     exit();
 }
 
